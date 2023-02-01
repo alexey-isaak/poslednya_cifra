8cfd5725281298b086f8c630bc3df84378ecace9
Alex Isaak <gafar@xaker.ru>
2023-02-01 20:33:06 +0300

    Itogovy script

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+ 
+namespace poslednya_cifra
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите трехзначное число: ");
+            int chislo = Convert.ToInt32(Console.ReadLine());
+            string chislo_string = Convert.ToString(chislo);
+
+            Console.WriteLine("Последняя цифра этого числа: " + chislo_string[chislo_string.Length - 1]);
+        }
+    }
+}
